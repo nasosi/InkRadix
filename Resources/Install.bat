e16d9767e7ecde3a077f9ba2b9d56462ead2d4c4
Nasos <2938701+nasosi@users.noreply.github.com>
2026-04-01 20:01:10 -0400

    Add install script

---
+++ Resources/Install.bat
@@ -0,0 +1,25 @@
+@echo off
+
+set "CUR_DIR=%CD%"
+set "TARGET_DIR=%APPDATA%\inkscape\extensions\nasos.inkradix"
+
+if not exist "%TARGET_DIR%" (
+    mkdir "%TARGET_DIR%"
+)
+cd ..
+
+copy /Y "InkRadix.inx" "%TARGET_DIR%"
+copy /Y "InkRadix.py" "%TARGET_DIR%"
+copy /Y "info.json" "%TARGET_DIR%"
+copy /Y "LICENSE" "%TARGET_DIR%"
+copy /Y "README.md" "%TARGET_DIR%"
+
+cd "%CUR_DIR%"
+
+echo.
+echo InkRadix files installed to:
+echo %TARGET_DIR%
+echo.
+echo Please restart Inkscape to load the extension.
+
+pause
